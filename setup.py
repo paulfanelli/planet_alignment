c9ff0b0f6e09f7099c8dcc4f55510bf82e0c6275
Paul Fanelli <paul.fanelli@gmail.com>
2015-06-25 21:49:31 -0400

    Initial project structure commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='planet_alignment',
+    version='1.0.0',
+    packages=['planet_alignment', 'planet_alignment.test'],
+    url='https://github.com/paulfanelli/planet_alignment.git',
+    license='MIT',
+    author='pfanelli',
+    author_email='paul.fanelli@gmail.com',
+    description='Planet Alignment program'
+)
