a4b410d133bf4ae026986de66a4ad004d707d706
Paul Fanelli <paul.fanelli@gmail.com>
2015-06-26 20:32:05 -0400

    path utility to get all of the project dirs.

---
+++ planet_alignment/test/utils/path.py
@@ -0,0 +1,49 @@
+"""
+.. module:: path
+   :platform: linux
+   :synopsis: Module to test getting all of the various project directories.
+
+.. moduleauthor:: Paul Fanelli <paul.fanelli@gmail.com>
+
+.. modulecreated:: 6/26/15
+
+"""
+from os.path import join
+from planet_alignment.utils import path
+
+
+def test_get_projects_dir():
+    """
+    Only uncomment to test on my system, leave commented out so others can run all the tests.
+    Once I've verified that it works, I can now use it below to get the projects dir, which will
+    also include the user's home dir.
+    """
+    # assert path.get_projects_dir() == '/home/pfanelli/python-devel'
+    pass
+
+
+PROJECTS_DIR = path.get_projects_dir()
+
+
+def test_get_root_dir():
+    assert path.get_root_dir() == join(PROJECTS_DIR, 'planet_alignment')
+
+
+def test_get_bin_dir():
+    assert path.get_bin_dir() == join(PROJECTS_DIR, 'planet_alignment', 'bin')
+
+
+def test_get_etc_dir():
+    assert path.get_etc_dir() == join(PROJECTS_DIR, 'planet_alignment', 'etc')
+
+
+def test_get_src_dir():
+    assert path.get_src_dir() == join(PROJECTS_DIR, 'planet_alignment', 'planet_alignment')
+
+
+def test_get_test_dir():
+    assert path.get_test_dir() == join(PROJECTS_DIR, 'planet_alignment', 'planet_alignment', 'test')
+
+
+def test_get_test_etc_dir():
+    assert path.get_test_etc_dir() == join(PROJECTS_DIR, 'planet_alignment', 'planet_alignment', 'test', 'etc')
---
+++ planet_alignment/utils/path.py
@@ -0,0 +1,47 @@
+"""
+.. module:: path
+   :platform: linux
+   :synopsis: Module to get all of the various project directories.
+
+.. moduleauthor:: Paul Fanelli <paul.fanelli@gmail.com>
+
+.. modulecreated:: 6/26/15
+
+"""
+from os.path import dirname, join
+
+PROJECTS_DIR = dirname(dirname(dirname(dirname(__file__))))
+ROOT_DIR = dirname(dirname(dirname(__file__)))
+BIN_DIR = join(ROOT_DIR, 'bin')
+ETC_DIR = join(ROOT_DIR, 'etc')
+SRC_DIR = dirname(dirname(__file__))
+TEST_DIR = join(SRC_DIR, 'test')
+TEST_ETC_DIR = join(TEST_DIR, 'etc')
+
+
+def get_projects_dir():
+    return PROJECTS_DIR
+
+
+def get_root_dir():
+    return ROOT_DIR
+
+
+def get_bin_dir():
+    return BIN_DIR
+
+
+def get_etc_dir():
+    return ETC_DIR
+
+
+def get_src_dir():
+    return SRC_DIR
+
+
+def get_test_dir():
+    return TEST_DIR
+
+
+def get_test_etc_dir():
+    return TEST_ETC_DIR
