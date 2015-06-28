9ddd529c0726e046a94c563e1fa1a94c78c7830b
Paul Fanelli <paul.fanelli@gmail.com>
2015-06-28 16:39:52 -0400

    Add the run method of app along with a test.

--- planet_alignment/app/app.py
+++ planet_alignment/app/app.py
@@ -19,3 +19,47 @@ class App(object):
         self._system_data = system_data
         self._plugins = plugins
         self._time = time
+
+    def run(self):
+        result_retval = []
+
+        for plugin_path in self._plugins:
+            plugin_inst = self._plugins.get_plugin_instance_by_path(plugin_path)
+            plugin_name = self._plugins.get_plugin_name_by_path(plugin_path)
+
+            plugin_str = ''
+            unique_aligned_list = []
+
+            for x in self._system_data:
+
+                aligned_list = []
+                for y in self._system_data:
+
+                    # don't compare the planets to themselves
+                    if x.name == y.name:
+                        continue
+
+                    try:
+                        result = plugin_inst.are_planets_aligned(x, y, self._time)
+                        if result:
+                            if x.name not in aligned_list:
+                                aligned_list.append(x.name)
+                            aligned_list.append(y.name)
+                            aligned_list.sort()
+                    except AttributeError as ae:
+                        print("ERROR: {}: {}".format(plugin_path, ae))
+                    except Exception as e:
+                        print("ERROR: Unknown error {}".format(e))
+
+                if aligned_list:
+                    if aligned_list not in unique_aligned_list:
+                        unique_aligned_list.append(aligned_list)
+
+                        plugin_str += plugin_name + ': '
+                        aligned_str = ', '.join(*unique_aligned_list)
+                        plugin_str += aligned_str + '\n'
+
+            if plugin_str:
+                result_retval.append(plugin_str)
+
+        return result_retval
--- planet_alignment/test/app/app.py
+++ planet_alignment/test/app/app.py
@@ -25,6 +25,15 @@ def fix_app():
     return App(sd, plugins, 0)
 
 
+@pytest.fixture(scope='module')
+def fix_align1():
+    config_file = constants.TEST_SYSTEM_YAML
+    data = BunchParser().parse(config_file)
+    sd = SystemData(data)
+    plugins = PluginsManager(constants.TEST_PLUGIN_LIST_ALIGN1)
+    return App(sd, plugins, 0)
+
+
 def test_app(fix_app):
     assert isinstance(fix_app._system_data, SystemData)
     assert len(fix_app._system_data) == 3
@@ -32,3 +41,14 @@ def test_app(fix_app):
     assert len(fix_app._plugins) == 2
     assert isinstance(fix_app._time, int)
     assert fix_app._time == 0
+
+
+def test_app_run(fix_app, capsys):
+    fix_app.run()
+    out, err = capsys.readouterr()
+    assert "object has no attribute" in str(out)
+
+
+def test_align1_run(fix_align1):
+    r = fix_align1.run()
+    assert r[0] == 'align1: planet-A, planet-B\n'
