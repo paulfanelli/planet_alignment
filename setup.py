b83490913fc82e955e83011493f6a8dcb62e8ef7
Paul Fanelli <paul.fanelli@gmail.com>
2015-06-28 23:22:18 -0400

    Use setuptools instead of distribute. Add missing packages.

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+# Utility function to read the README file, used for the long desc.
+def read(fn):
+    return open(os.path.join(os.path.dirname(__file__), fn)).read()
+
 
 setup(
     name='planet_alignment',
     version='1.0.0',
-    packages=['planet_alignment', 'planet_alignment.test'],
+    packages=[
+        'planet_alignment',
+        'planet_alignment.app',
+        'planet_alignment.cmd',
+        'planet_alignment.config',
+        'planet_alignment.data',
+        'planet_alignment.mgr',
+        'planet_alignment.plugins',
+        'planet_alignment.test',
+        'planet_alignment.utils'
+    ],
     url='https://github.com/paulfanelli/planet_alignment.git',
     license='MIT',
     author='Paul Fanelli',
     author_email='paul.fanelli@gmail.com',
     description='Planet Alignment program',
+    long_description=read('README'),
     requires=['bunch', 'zope.interface', 'PyYAML'],
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'planet_alignment = planet_alignment.__main__:main'
         ]
-    }
+    },
+    include_package_data=True
 )
